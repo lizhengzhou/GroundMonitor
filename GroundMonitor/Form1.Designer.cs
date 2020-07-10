6c8ed9153f4885eb9d55f85f3242072d862fc1a8
lizz <374494848@qq.com>
2020-07-10 14:32:44 +0800

    添加项目文件。

---
+++ GroundMonitor/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace GroundMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pb_device4 = new System.Windows.Forms.PictureBox();
+            this.pb_device3 = new System.Windows.Forms.PictureBox();
+            this.pb_device2 = new System.Windows.Forms.PictureBox();
+            this.pb_device1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lb_msg = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_device4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_device3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_device2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_device1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pb_device4);
+            this.groupBox1.Controls.Add(this.pb_device3);
+            this.groupBox1.Controls.Add(this.pb_device2);
+            this.groupBox1.Controls.Add(this.pb_device1);
+            this.groupBox1.Controls.Add(this.pictureBox4);
+            this.groupBox1.Controls.Add(this.pictureBox3);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(41, 67);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(726, 240);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "检测车间";
+            // 
+            // pb_device4
+            // 
+            this.pb_device4.Image = global::GroundMonitor.Properties.Resources.green;
+            this.pb_device4.Location = new System.Drawing.Point(637, 66);
+            this.pb_device4.Name = "pb_device4";
+            this.pb_device4.Size = new System.Drawing.Size(51, 54);
+            this.pb_device4.TabIndex = 7;
+            this.pb_device4.TabStop = false;
+            // 
+            // pb_device3
+            // 
+            this.pb_device3.Image = global::GroundMonitor.Properties.Resources.green;
+            this.pb_device3.Location = new System.Drawing.Point(460, 66);
+            this.pb_device3.Name = "pb_device3";
+            this.pb_device3.Size = new System.Drawing.Size(51, 54);
+            this.pb_device3.TabIndex = 6;
+            this.pb_device3.TabStop = false;
+            // 
+            // pb_device2
+            // 
+            this.pb_device2.Image = global::GroundMonitor.Properties.Resources.green;
+            this.pb_device2.Location = new System.Drawing.Point(289, 66);
+            this.pb_device2.Name = "pb_device2";
+            this.pb_device2.Size = new System.Drawing.Size(51, 54);
+            this.pb_device2.TabIndex = 5;
+            this.pb_device2.TabStop = false;
+            // 
+            // pb_device1
+            // 
+            this.pb_device1.Image = global::GroundMonitor.Properties.Resources.green;
+            this.pb_device1.Location = new System.Drawing.Point(119, 66);
+            this.pb_device1.Name = "pb_device1";
+            this.pb_device1.Size = new System.Drawing.Size(51, 54);
+            this.pb_device1.TabIndex = 4;
+            this.pb_device1.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(588, 126);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 84);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(411, 126);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 84);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(240, 126);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 84);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(70, 126);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 84);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lb_msg
+            // 
+            this.lb_msg.AutoSize = true;
+            this.lb_msg.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lb_msg.Location = new System.Drawing.Point(41, 359);
+            this.lb_msg.Name = "lb_msg";
+            this.lb_msg.Size = new System.Drawing.Size(43, 21);
+            this.lb_msg.TabIndex = 2;
+            this.lb_msg.Text = "#0 ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lb_msg);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "接地监控";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_device4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_device3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_device2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_device1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pb_device4;
+        private System.Windows.Forms.PictureBox pb_device3;
+        private System.Windows.Forms.PictureBox pb_device2;
+        private System.Windows.Forms.PictureBox pb_device1;
+        private System.Windows.Forms.Label lb_msg;
+    }
+}
+
---
+++ GroundMonitor/Form1.cs
@@ -0,0 +1,159 @@
+﻿using GroundMonitor.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace GroundMonitor
+{
+    public partial class Form1 : Form
+    {
+        string Ip = "10.10.100.254";
+        Thread _thread_connect;
+        Thread _thread_recieve;
+        static TcpClient client;
+
+        PictureBox[] Devices;
+        static bool[] DeviceStatus;
+        static bool IsClosing = false;
+
+        static object _pb_lock = new object();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(ConfigurationManager.AppSettings["ServerIP"]))
+            {
+                Ip = ConfigurationManager.AppSettings["ServerIP"];
+            }
+
+            Devices = new PictureBox[4] { pb_device1, pb_device2, pb_device3, pb_device4 };
+            DeviceStatus = new bool[4] { true, true, true, true };
+            client = new TcpClient();
+
+            _thread_recieve = new Thread(Recieve);
+            _thread_recieve.Start();
+
+            _thread_connect = new Thread(Connect);
+            _thread_connect.Start();
+        }
+
+        void Connect()
+        {
+            while (!IsClosing)
+            {
+                if (!client.Connected)
+                {
+                    try
+                    {
+                        client.Connect(Ip, 8899);
+
+                        UpdateMsg("连接成功");
+                    }
+                    catch
+                    {
+                        UpdateMsg($"连接失败->{Ip}:8899");
+                    }
+                }
+
+                Thread.Sleep(1000);
+            }
+
+        }
+
+        void Recieve()
+        {
+            while (!IsClosing)
+            {
+                try
+                {
+                    if (client.Connected && client.Available > 0)
+                    {
+                        var stream = client.GetStream();
+                        var buf = new byte[client.Available];
+                        stream.Read(buf, 0, client.Available);
+
+                        UpdateMsg(BitConverter.ToString(buf));
+
+                        handleData(buf);
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    UpdateMsg(ex.Message);
+                }
+
+                Thread.Sleep(500);
+            }
+        }
+
+        void handleData(byte[] buf)
+        {
+            if (buf.Length > 13)
+            {
+                for (int i = 0; i + 13 < buf.Length; i++)
+                {
+                    if (buf[i] == 0xa5 && buf[i + 1] == 0x5a)
+                    {
+                        var index = buf[i + 9];
+                        var status = buf[10] == 0x01;
+
+                        UpdateStatus(index, status);
+
+                    }
+                }
+            }
+        }
+
+        void UpdateMsg(string message)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action<string>((msg) =>
+                {
+                    lb_msg.Text = "#0 " + msg;
+                }), message);
+            }
+            else
+            {
+                lb_msg.Text = "#0 " + message;
+            }
+        }
+
+        void UpdateStatus(int index, bool status)
+        {
+            if (DeviceStatus.Length >= index && DeviceStatus[index - 1] != status)
+            {
+                var statusDesc = status ? "正常" : "异常";
+                UpdateMsg($"设备{index}接地{statusDesc}");
+
+                var pic = status ? Resources.green : Resources.red;
+
+                lock (_pb_lock)
+                {
+                    Devices[index - 1].Image = pic;
+                    DeviceStatus[index - 1] = status;
+                }
+
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            IsClosing = true;
+        }
+    }
+}
